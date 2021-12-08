5072fd1128dd4fac5e4ad7c4ea9487937792d816
limaoxing <670148403@qq.com>
2021-12-08 11:50:58 +0800

    first commit

---
+++ XiaoFenShu02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XiaoFenShu02
+//
+//  Created by limaoxing on 2021/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
